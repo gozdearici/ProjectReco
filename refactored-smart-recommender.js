724c36eca2825af0f17a41629a90938cfcadb950
Gozde Nur Yildiz <gozde.yildiz@useinsider.com>
2023-08-17 23:41:58 +0300

    final refactored recommender project

--- refactored-smart-recommender.js
+++ refactored-smart-recommender.js
@@ -1,17 +1,13 @@
 ((self) => {
     'use strict';
 
-    const config = {
-        leftArrow: '<',
-        rightArrow: '>',
-    };
-
     const classes = {
         sliderWrapper: 'ins-slider-wrapper',
         productsTitle: 'ins-products-title',
         productContainer: 'ins-product-container',
-        leftArrowButton: 'ins-left-arrow-button',
-        rightArrowButton: 'ins-right-arrow-button',
+        arrowButton: 'ins-arrow-button',
+        leftArrow: 'ins-left-arrow',
+        rightArrow: 'ins-right-arrow',
         productCard: 'ins-product-card',
         productFrame: 'ins-product-frame',
         productImage: 'ins-product-image',
@@ -29,12 +25,11 @@
     self.init = async () => {
         initializeJQuery();
         self.reset();
-        self.buildHTML();
+        self.buildHtml();
         const products = await fetchProducts();
         products.forEach(product => {
-            const productPrice = product.price === undefined ? 'Out Of Stock' : product.price + ' TL'
-            console.log()
-            self.buildInnerHTML(product.img, product.url, product.name, productPrice);
+            const productPrice = product.price === undefined ? 'Out Of Stock' : `${product.price} TL`;
+            self.buildProductHtml(product.img, product.url, product.name, productPrice);
         });
         self.buildCss();
         self.buildEventListeners();
@@ -57,96 +52,91 @@
     };
 
     self.buildCss = () => {
-        const { sliderWrapper, productsTitle , productContainer, rightArrowButton, leftArrowButton, productCard,
+        const { sliderWrapper, productsTitle , productContainer, arrowButton, rightArrow, leftArrow, productCard,
             productFrame, productImage, productInfo, productShortDescription, productPrice} = selectors;
 
         const style =
         `${sliderWrapper} {
-            position : static;
-            width : 80%;
-            margin : 0 auto;
+            position: static;
+            width: 80%;
+            margin: 0 auto;
         }
         ${productsTitle} {
-            font-size : 25px;
-            font-weight : 500;
-            margin-bottom : 40px;
-            text-transform : uppercase;
+            font-size: 25px;
+            font-weight: 500;
+            margin-bottom: 40px;
+            text-transform: uppercase;
         }
 
         ${productContainer} {
-            scroll-snap-type : x mandatory;
-            display : grid;
-            grid-auto-columns : 20%;
-            align-items : center;
-            grid-auto-flow : column;
-            margin-left : 35px;
-            overflow :  hidden;
-            list-style : none;
-            scroll-behavior : smooth;
+            scroll-snap-type: x mandatory;
+            display: grid;
+            grid-auto-columns: 20%;
+            align-items: center;
+            grid-auto-flow: column;
+            margin-left: 35px;
+            overflow: hidden;
+            list-style: none;
+            scroll-behavior: smooth;
         }
-        ${rightArrowButton} {
+        ${arrowButton} {
             background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #fff 80%);
-            border : none;
+            border: none;
             font-size: 50px;
-            cursor : pointer;
-            position : absolute;
+            cursor: pointer;
+            position: absolute;
             z-index: 8;
             margin-top: 3em;
+        }
+        ${rightArrow} {
             right: 5%;
         }
-        ${leftArrowButton} {
-            background: linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #fff 80%);
-            border : none;
-            font-size: 50px;
-            cursor : pointer;
-            position : absolute;
-            z-index: 8;
-            margin-top: 3em;
+        ${leftArrow} {
             left: 8%;
         }
         ${productCard} {
-            scroll-snap-align : start;
-            flex : grid;
-            object-fit : cover;
-            width : 250px;
-            height : 450px;
-            cursor : pointer;
+            scroll-snap-align: start;
+            flex: grid;
+            object-fit: cover;
+            width: 250px;
+            height: 450px;
+            cursor: pointer;
         }
         ${productFrame} {
-            position : relative;
-            width : 100%;
+            position: relative;
+            width: 100%;
         }
         ${productImage} {
-            width : 100%;
-            height : 100%;
-            object-fit : cover;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
         }
         ${productInfo} {
-            display : flex;
-            flex-direction : column;
-            gap : 10px;
-            width : 100%;
-            padding-top : 10px;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            width: 100%;
+            padding-top: 10px;
         }
         ${productShortDescription} {
-            width : 100%;
-            height : 20px;
-            line-height : 20px;
-            opacity : 0.5;
+            width: 100%;
+            height: 20px;
+            line-height: 20px;
+            opacity: 0.5;
         }
         ${productPrice} {
-            font-weight : 700;
-            font-size : 20px;
+            font-weight: 700;
+            font-size: 20px;
         }
         @media (max-width: 1024px) and (min-width: 768px) {
             ${productContainer} {
-                grid-auto-columns : 33%;
+                grid-auto-columns: 33%;
             }
 
         }
         @media screen and (max-width: 768px) {
             ${productContainer} {
-                grid-auto-columns : 100%;
+                grid-auto-columns: 100%;
             }  
         }
     `;
@@ -154,26 +144,24 @@
         Insider.dom('<style>').addClass(classes.style).html(style).appendTo('head');
     };
 
-    self.buildHTML = () => {
-        const { sliderWrapper, productsTitle ,productContainer, leftArrowButton, rightArrowButton} = classes;
-        const { leftArrow, rightArrow } = config;
+    self.buildHtml = () => {
+        const { sliderWrapper, productsTitle, productContainer, arrowButton, leftArrow, rightArrow } = classes;
 
         const outerHtml =
         `<section class=${sliderWrapper}>
             <h2 class=${productsTitle}>You might also like</h2>
-            <button class=${leftArrowButton}>${leftArrow}</button>
-            <button class=${rightArrowButton}>${rightArrow}</button>
+            <button class="${leftArrow} ${arrowButton}">${'<'}</button>
+            <button class="${rightArrow} ${arrowButton}">${'>'}</button>
             <div class=${productContainer}></div>
         </section>`;
 
         Insider.dom('.footer-content').append(outerHtml);
     };
 
-    self.buildInnerHTML = (productImg, productUrl, productName, price) => {
-        const {productContainer, productCard, productFrame, productImage, productInfo, productShortDescription, productPrice} = classes;
-        let innerHtml = '';
+    self.buildProductHtml = (productImg, productUrl, productName, price) => {
+        const { productContainer, productCard, productFrame, productImage, productInfo, productShortDescription, productPrice } = classes;
 
-        innerHtml += `
+        const innerHtml = `
             <div class=${productCard}>
                 <div class=${productFrame}>
                     <a class=${productImage} href =${productUrl}>
@@ -190,18 +178,18 @@
     };
 
     self.buildEventListeners = () => {
-        const { productCard, productContainer, rightArrowButton, leftArrowButton } = selectors;
+        const { productCard, productContainer, rightArrow, leftArrow } = selectors;
 
         const card = Insider.dom(productCard).nodes[0];
         const container = Insider.dom(productContainer).nodes[0];
-        const nextButton = Insider.dom(rightArrowButton)
-        const prevButton = Insider.dom(leftArrowButton)
+        const nextButton = Insider.dom(rightArrow)
+        const prevButton = Insider.dom(leftArrow)
 
-        Insider.eventManager.once('click.left:arrow:button', nextButton.selector, () => {
+        Insider.eventManager.once('click.left:arrow', nextButton.selector, () => {
             container.scrollLeft += card.clientWidth;
         });
 
-        Insider.eventManager.once('click.right:arrow:button', prevButton.selector, () => {
+        Insider.eventManager.once('click.right:arrow', prevButton.selector, () => {
             container.scrollLeft -= card.clientWidth
         });
 
